49b69f700efce9921d1fe02efe6869c813ecd488
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-07-31 00:07:08 +0545

    Add libreoffice & time command queries

--- queryprocessor.py
+++ queryprocessor.py
@@ -9,5 +9,18 @@ def generateRequestDict(request_index, request_type, spoken_text):
 def queryProcessor(spoken_text, questions):
     if(spoken_text in questions):
         return generateRequestDict(questions.index(spoken_text), 'text_queries', spoken_text)
+
+    elif 'powerpoint' in spoken_text:
+        return generateRequestDict(questions.index("powerpoint"), 'command_queries', "powerpoint")
+    
+    elif 'excel' in spoken_text:
+        return generateRequestDict(questions.index("calc"), 'command_queries', "excel")
+    
+    elif 'writer' in spoken_text:
+        return generateRequestDict(questions.index("writer"), 'command_queries', "writer")
+
+    elif 'time' in spoken_text:
+        return generateRequestDict(questions.index("time"), 'command_queries', "time")
+
     elif(len(spoken_text) > 0):
         return generateRequestDict(0, 'not_found', spoken_text)
--- queryresponder.py
+++ queryresponder.py
@@ -1,4 +1,17 @@
 import audioprocessor as ap
+import os
+import time
+
+
+def checkCommands(text):
+    if(text == "powerpoint"):
+        os.system("libreoffice --draw")
+    elif(text == "excel"):
+        os.system("libreoffice --calc")
+    elif(text == "writer"):
+        os.system("libreoffice --writer")
+    elif(text == "time"):
+        ap.say(time.ctime())
 
 
 def queryResponder(request_input, answers):
@@ -9,5 +22,12 @@ def queryResponder(request_input, answers):
         reply = reply.replace(
             "{userName}", request_input['userName'])
         ap.say(reply)
+        checkCommands(request_input['text'])
+
+    elif(request_input['type'] == 'command_queries'):
+        answer = answers[request_input['index']]
+        ap.say(answer)
+        checkCommands(request_input['text'])
+
     else:
-        ap.say('Sorry, i didn\'t get you. I still have to learn a lot of things.')
+        ap.say('Sorry, i didn\'t get you.')
